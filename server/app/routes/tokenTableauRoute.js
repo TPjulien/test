67304fd75c97904b6d2dda85bc34d511fb59634b
mahefa <nmahefa@gmail.com>
2016-01-19 14:08:52 +0100

    mise a jour du server

--- server/app/routes/tokenTableauRoute.js
+++ server/app/routes/tokenTableauRoute.js
@@ -1,5 +1,7 @@
 var mysql       = require('mysql');
 var PythonShell = require('python-shell');
+var http        = require('http');
+var http-post   = require('http-post');
 
 module.exports = function(router, connection) {
     function getArgs(site, client) {
@@ -12,11 +14,17 @@ module.exports = function(router, connection) {
 
     router.route('/getTicket')
         .post(function(req, res) {
-            PythonShell.run('ticket.py', getArgs(req.body.site, req.body.username), function(err, results) {
-                if (err)
-                    res.sendStatus(404, "unable to execute query");
-                else
-                    res.json(results);
-            });
+            http.post('http://data.travelplanet.fr/trusted', { username: 'GuillaumeNaturel', target_site: 'anses'}, function(res) {
+                response.setEncoding('utf8');
+                res.on('data', function(chunk) {
+                  console.log(chunk);
+                })
+            })
+            // PythonShell.run('ticket.py', getArgs(req.body.site, req.body.username), function(err, results) {
+            //     if (err)
+            //         res.sendStatus(404, "unable to execute query");
+            //     else
+            //         res.json(results);
+            // });
         })
 }
