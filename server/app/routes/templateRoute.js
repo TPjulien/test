ed518c23eb7c4a9f125f23c2a72cb7a8993888e7
defaultuser <defaultuser@portail.test>
2016-01-25 15:06:51 +0100

    mise a jour

--- server/app/routes/templateRoute.js
+++ server/app/routes/templateRoute.js
@@ -7,6 +7,7 @@ module.exports = function(router, connection) {
     router.route('/view/:user/:site')
         .get (function(req, res) {
             var completed_requests = 0;
+	    var second_requests = 0;
             var element      = {};
             var final_object = [];
             var query = "SELECT * FROM ?? WHERE ?? = ?";
@@ -16,10 +17,36 @@ module.exports = function(router, connection) {
                 if (err) {
                     res.json({ message: 'error !'})
                 } else {
-
-                  for (i = 0; i <= rows.length; i++) {
-                      request.post({url: 'http://data.travelplanet.fr/trusted', { use,rname: req.params.user, target_site: req.params.site }}, function(err, body) {
-                          console.log(body);
+		  var token = [];
+		  var info  = [];
+		  var length = [];
+		  var height = [];
+		  var width = [];
+		  var name  = [];
+                  for (item in rows) {
+                      request.post('http://data.travelplanet.fr/trusted', {form:{ username: req.params.user, target_site: req.params.site }}, function(err, resultat, body) {
+			  info.push(rows[completed_requests].path);
+			  token.push(body);
+			  length.push("null");
+			  width.push("null");
+			  height.push(rows[completed_requests].height);
+			  name.push(rows[completed_requests].name);
+			  completed_requests++;
+			  if (completed_requests == rows.length)
+			      var afterResult = {};
+			      var object_parsed = [];
+			      for (getinfo in info) {
+				  element.info  = info[getinfo];
+				  element.token = token[getinfo];
+				  element.length = length[getinfo];
+				  element.width = width[getinfo];
+				  element.name  = name[getinfo];
+				  object_parsed.push(element);
+				  second_requests++;
+				  if(second_requests == (rows.length + 1)) {
+				      res.json({info: info, token: token, width: width, length:length, name: name, height: height});
+				  }
+			      }
                       });
                       // http.post('http://data.travelplanet.fr/trusted', { use,rname: req.params.user, target_site: req.params.site }, function(res) {
                       //     console.log(res.body);
