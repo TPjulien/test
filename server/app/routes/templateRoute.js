1423a842bc3dcd3222872cde74c80dfd25618da6
mahefa <nmahefa@gmail.com>
2016-01-25 12:49:52 +0100

    correction indentation

--- server/app/routes/templateRoute.js
+++ server/app/routes/templateRoute.js
@@ -3,55 +3,40 @@ var http_post = require('http-post');
 
 module.exports = function(router, connection) {
 
-    function getTokens(user, site, length, callback) {
-        var element      = {};
-        var final_object = [];
-        for (i=0; i < (length - 1); i++) {
-          http_post('http://data.travelplanet.fr/trusted', { username: user, target_site: site }, function(result) {
-            result.setEncoding('utf8');
-            result.on('data', function(chunk) {
-                    element.path  = chunk;
-                    element.info   = rows[i].path;
-                    element.length = "null";
-                    element.height = "null";
-                    element.width  = "null";
-                    element.name   = rows[i].name;
-                    final_object.push(element);
-                })
-            })
-        }
-        callback(final_object, 404);
-        // var query = "SELECT ?? FROM ?? WHERE ?? = ?";
-        // var table = [table_password, table_login, table_username, loginUser];
-        // query     = mysql.format(query, table);
-        // connection.query(query, function(err, rows) {
-        //     if (err) {
-        //         callback(err, 404);
-        //     } else {
-        //         callback(null, rows);
-        //     }
-        // })
+    function getToken(user, site, length, callback) {
+      for (i=0; i < (length - 1); i++) {
+        http_post('http://data.travelplanet.fr/trusted', { username: user, target_site: site }, function(result) {
+          console.log(result);
+          // result.setEncoding('utf8');
+          // result.on('data', function(chunk) {
+          //         element.path  = chunk;
+          //         element.info   = rows[i].path;
+          //         element.length = "null";
+          //         element.height = "null";
+          //         element.width  = "null";
+          //         element.name   = rows[i].name;
+          //         final_object.push(element);
+          //     })
+          })
+          callback(result, 200);
+      }
     }
 
 
     router.route('/view/:user/:site')
         .get (function(req, res) {
+            var element      = {};
+            var final_object = [];
             var query = "SELECT * FROM ?? WHERE ?? = ?";
             var table = ['template_path', 'user', req.params.user];
-
             query     = mysql.format(query, table);
             connection.query(query, function(err, rows) {
                 if (err) {
                     res.json({ message: 'error !'})
                 } else {
-                    getTokens(req.params.user, req.params.site, function(err, data) {
-                        console.log(data);
-                    });
-                    // for (i=0; i < (rows.length - 1); i++) {
-                    //
-                  	// 		// res.json(final_object);
-                    // })
-                }
+                  getToken(req.params.user, req.params.site, function(err, data) {
+                      console.log(data);
+                  })
               }
             })
         })
