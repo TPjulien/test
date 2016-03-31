5cbe1c312b4f1dd947cb8ecdbbd3390ed87169b8
mahefa <nmahefa@gmail.com>
2016-03-31 17:48:08 +0200

    mise a jour de la requete

--- server/app/routes/templateRoute.js
+++ server/app/routes/templateRoute.js
@@ -20,6 +20,7 @@ module.exports = function(router, connection) {
                     res.status(404).send("Not found !");
                 } else {
                   if (rows[0].embed_content_type == "Tableau") {
+                    // Get only the necessary data (generic data)
                     var query_two = "SELECT * FROM ?? WHERE ?? = ? AND ?? = ? AND ?? = ?";
                     var table_two = ['tableau_view_info', 'view_id', req.params.view, 'site_id', req.params.customer, 'auth_user_role', req.params.auth_role];
                     query_two     = mysql.format(query_two, table_two);
@@ -32,13 +33,14 @@ module.exports = function(router, connection) {
                           resultObject = {};
                           counter     = 0;
                           for (items in rows_tableau) {
+                              // get token for each tableau in row
                               request.post('http://' + rows_tableau[counter].tableau_server_url + '/trusted', {form:{ username: req.params.user, target_site: req.params.site }}, function(err, resultat, body) {
                                   resultObject[counter] = {  "site_id"             : rows[counter].site_id,
                                                              "view_id"             : rows[counter].view_id,
                                                              "embed_id"            : rows[counter].embed_id,
                                                              "path_to_view"        : rows_tableau[counter].path_to_view,
-                                                             "embed_width"         : rows_tableau[counter].embed_width,
-                                                             "embed_height"        : rows_tableau[counter].embed_height,
+                                                             "embed_width"         : rows[counter].embed_width,
+                                                             "embed_height"        : rows[counter].embed_height,
                                                              "embed_position"      : rows[counter].embed_position,
                                                              "embed_content_type"  : rows[counter].embed_content_type,
                                                              "tableau_customer_id" : rows_tableau[counter].tableau_customer_id,
@@ -53,8 +55,8 @@ module.exports = function(router, connection) {
                           }
                         }
                     });
-                  } else {
-                      // if we need another view
+                    } else {
+                      // Generic request if we don't find Tableau
                       var table_name  = rows[0].embed_content_type + "_view_info";
                       var query_three = "SELECT * FROM ?? WHERE ?? = ? AND ?? = ? AND ?? = ?";
                       var table_three = [table_name, 'view_id', req.params.view, 'site_id', req.params.customer, 'auth_user_role', req.params.auth_role];
