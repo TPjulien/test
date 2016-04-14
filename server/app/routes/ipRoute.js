90206819600d8f3072b0d212792a0b6958911c78
mahefa <nmahefa@gmail.com>
2016-04-14 13:10:53 +0200

    ajout d'une localisation d'adresse ip

--- server/app/routes/ipRoute.js
+++ server/app/routes/ipRoute.js
@@ -1,10 +1,14 @@
 var mysql = require('mysql');
+var geoip = require('geoip-lite');
+
 
 module.exports = function(router, connection) {
     router.route('/rules/ip')
         .get (function(req, res) {
             var getIp = req.connection.remoteAddress;
-            res.json(getIp);
+            res.json({"ip" : getIp,
+                      "dataInfo": geoip.lookup(getIp)
+            });
             // var query = "SELECT * from ?? WHERE ?? = ? AND ?? = ?";
             // var table = ['rules_filter_info', 'client_id', req.params.client_id, 'user_id', req.params.user_id];
             // query     = mysql.format(query, table);
