1b906b738ecfcd50d0695b5ce2c20f57745b8dd7
mahefa <nmahefa@gmail.com>
2016-04-22 11:05:11 +0200

    ajout de la requete

--- server/app/routes/pdfRoute.js
+++ server/app/routes/pdfRoute.js
@@ -133,12 +133,12 @@ module.exports = function(router, connection) {
                   res.status(404).send('unable to execute query');
               } else {
                 var query = "SELECT ??,?? from ?? ";
-                if (req.params.num_invoice != 'none' && req.params.num_commande == "none")
+                if (req.params.num_invoice != 'none')
                   var query  = query + "WHERE NUM_INVOICE LIKE '%" + req.params.num_invoice + "%' ";
                 if (req.params.num_commande != "none" && req.params.num_invoice != "none")
                    var query = query + "AND NUM_COMMANDE LIKE '%" + req.params.num_commande + "%' ";
                 else if (req.params.num_commande != "none" && req.params.num_invoice == "none")
-                   var query = query + "WHERE NUM_INVOICE LIKE '%" + req.params.num_commande + "%' ";
+                   var query = query + "WHERE COMMANDE LIKE '%" + req.params.num_commande + "%' ";
                 query = query + "LIMIT " + req.params.min + "," + max;
                 var table = ["NUM_INVOICE", "NUM_COMMANDE", "accelya.vue_juju"];
                 query = mysql.format(query, table);
