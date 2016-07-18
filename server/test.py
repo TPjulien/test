4444a7601bf5cfbb5602bcd71bcfc87d0d255391
defaultuser <defaultuser@portail.test>
2016-07-18 16:49:56 +0200

    mise a jour du projet

--- server/test.py
+++ server/test.py
@@ -2,6 +2,9 @@ import requests
 import sys
 import codecs
 import re
+import string
+import htmlmin
+from slimmer import html_slimmer
 
 sys.stdout = codecs.getwriter('utf8')(sys.stdout)
 sys.stderr = codecs.getwriter('utf8')(sys.stderr)
@@ -12,12 +15,21 @@ def toto():
                                                                                        'LANGUAGE':          'FR',
                                                                                        'LOGIN_TYPE':        'SSO',
                                                                                        'PASSWORD':          'travel2014',
-                                                                                       'BOOKING_FLOW_TYPE': 'MODIFY'}, allow_redirects=False)
-    print(r.status_code, r.reason)
-    print(r.text)
+ 
+                                                                                       'BOOKING_FLOW_TYPE': 'MODIFY'}, allow_redirects=True)
+    #r.encoding
+    #print(r.text[0])
+    #print(r.text)
 
     # regex url
-    urls = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', r.text)
-    print(urls[0]);
-
+    #urls = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', r.text)
+    #print(urls[0]);
+    t = r.text;
+    
+    #print t.translate(string.maketrans("\n\t\r", " "))
+    #print("<div><h1>Putain de merde</h1></div>")
+    html = htmlmin.minify(t, remove_empty_space=True)
+    slimmer = html_slimmer(html.strip().replace('\n', ' ').replace('\t', ' ').replace('\r', ' '))
+    print(slimmer);
+    
 toto()
