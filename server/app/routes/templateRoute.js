a088d5c0f3bae3847921c0ddf826cacebd394721
mahefa <nmahefa@gmail.com>
2016-07-26 12:19:24 +0200

    valeur par défaut

--- server/app/routes/templateRoute.js
+++ server/app/routes/templateRoute.js
@@ -40,7 +40,7 @@ module.exports = function(router, connection) {
                           // la on regarde si c'est un tableau ou bien autre chose
                           if (result_embed_content_type.embed_content_type == "tableau") {
 
-                          } else if (result_embed_content_type.embed_content_type == "datatable") {
+                          } else if (result_embed_content_type[0].embed_content_type == "datatable") {
                               // si jamais le content_type c'est un datatable
                               var request_datatable = "SELECT * FROM ?? WHERE ?? =? AND ?? =?";
                               var table_datatable   = ["tp_control.datatable", "SITE_ID", site_id, "VIEW_ID", view_id];
@@ -53,6 +53,7 @@ module.exports = function(router, connection) {
                                   }
                               })
                           } else {
+                              console.log(result_embed_content_type[0]);
                               // dans tout les autres cas
                               res.send("Test");
                           }
