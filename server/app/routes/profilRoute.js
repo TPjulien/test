899b0695585cb77be03fb5216b373661c670b2dd
chrisRocc <christophe.rocc@gmail.com>
2016-08-01 17:29:16 +0200

    ajout preference siege avion

--- server/app/routes/profilRoute.js
+++ server/app/routes/profilRoute.js
@@ -178,6 +178,23 @@ module.exports = function(router, connection) {
             })
         })
     // route pour lister toutes les countries pour le phone
+    router.route('/airSeatingPref')
+        .get(function(req, res) {
+            var query_one = "SELECT DISTINCT ?? FROM ?? \
+                             WHERE ?? IS NOT NULL \
+                             AND ?? !='' ";
+            var table_one = ["profils.air_pref.AirSeatingPref", "profils.air_pref",
+                             "profils.AirSeatingPref",
+                             "profils.AirSeatingPref"];
+            query_one = mysql.format(query_one, table_one);
+            connection.query(query_one, function(err, rows) {
+                if (err)
+                    res.status(400).send(err);
+                else
+                    res.json(rows);
+            })
+        })
+    // route pour lister toutes les countries pour le phone
     router.route('/getCountry')
         .get(function(req, res) {
             var query_one = "SELECT ?? FROM ??";
