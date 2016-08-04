6dc4420eb9fc2912f1fa59fd200d58263d8f23c0
mahefa <nmahefa@gmail.com>
2016-08-04 17:22:49 +0200

    purée

--- server/app/routes/datatable.js
+++ server/app/routes/datatable.js
@@ -164,12 +164,12 @@ module.exports = function(router, connection) {
                         for (name in filters) {
                             if (name == 0) {
                                 if (filters[name]['value'].length == 2)
-                                    query_datatable += ' WHERE ' + filters[name]['column_name'] + " BETWEEN " + filters[name]['value'][0] + " AND " filter[name]['value'][0];
+                                    query_datatable += ' WHERE ' + filters[name]['column_name'] + " BETWEEN " + filters[name]['value'][0] + " AND " filters[name]['value'][0];
                                 else
                                     query_datatable += ' WHERE ' + filters[name]['column_name'] + " LIKE '%" + filters[name]['value'] + "%' ";
                             } else {
                                 if (filters[name]['value'].length == 2)
-                                    query_datatable += ' AND ' + filters[name]['column_name'] + " BETWEEN " + filters[name]['value'][0] + " AND " filter[name]['value'][0];
+                                    query_datatable += ' AND ' + filters[name]['column_name'] + " BETWEEN " + filters[name]['value'][0] + " AND " filters[name]['value'][0];
                                 else
                                     query_datatable += ' AND ' + filters[name]['column_name'] + " LIKE '%" + filters[name]['value'] + "%' ";
                             }
