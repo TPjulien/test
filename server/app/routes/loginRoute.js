914588b7b3ce222ce3d84584845db1ee3d7745c2
mahefa <nmahefa@gmail.com>
2016-08-16 18:09:51 +0200

    correction du shibboleth

--- server/app/routes/loginRoute.js
+++ server/app/routes/loginRoute.js
@@ -27,7 +27,7 @@ module.exports = function(router, connection) {
     passport.use(new SamlStrategy(
       {
         path :      '/loginProfils',
-        entryPoint: 'https://shibboleth-test.main.ad.rit.edu/idp/profile/SAML2/Redirect/SSO'
+        entryPoint: 'https://test.federation.renater.fr/idp/profile/SAML2/Redirect/SSO'
         // entryPoint: 'https://test.federation.renater.fr/idp/profile/SAML2/Redirect/SSO',
         issuer:     'passport-saml'
         // issuer:     'https://test.federation.renater.fr/idp/shibboleth'
