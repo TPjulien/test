01502581c4eda6f97a75a9f92e66034e538695ea
mahefa <nmahefa@gmail.com>
2016-08-17 15:07:15 +0200

    ajout des headers

--- server/server.js
+++ server/server.js
@@ -38,7 +38,11 @@ connection.connect(function(err) {
 // cors perso
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.header("Access-Control-Max-Age", '86400');
+    next();
 });
 
 // ajouter une whitelist pour Cors
