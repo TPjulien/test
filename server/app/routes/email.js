06de6aa566bbea0331829bb50f4e818b96fcf62c
ChrisRocch <christophe.rocc@gmail.com>
2016-09-15 18:31:08 +0200

    ajout requete infoReponse

--- server/app/routes/email.js
+++ server/app/routes/email.js
@@ -56,6 +56,24 @@ module.exports = function(router, connection) {
                   res.json(rows);
           })
       })
+  // route pour lister les infos mail
+  router.route('/infoReponse/:site_id/:uid/:billet_id')
+      .get(function(req, res) {
+          var query_one = "SELECT * FROM ??  \
+                            WHERE ?? = ?  AND ?? = ?? AND ?? = ? GROUP BY ??";
+          var table_one = ["tp_control.History_Email_WIP",
+                           "SITE_ID", req.params.site_id,
+                           "UID", req.params.uid,
+                           "BILLET_ID", req.params.billet_id,
+                           "BILLET_ID"];
+          query_one     = mysql.format(query_one, table_one);
+          connection.query(query_one, function(err, rows) {
+              if (err)
+                  res.status(400).send(err);
+              else
+                  res.json(rows);
+          })
+      })
   // route pour ajouter le mail en base
   router.route('/putHistoryMail')
       .post (function(req, res) {
