92002bb14c443528626de2c97baadd68bcb99b25
defaultuser <defaultuser@portail.test>
2016-10-11 17:22:46 +0200

    correction de la requete

--- server/app/routes/ipRoute.js
+++ server/app/routes/ipRoute.js
@@ -4,13 +4,14 @@ var mysql = require('mysql');
 module.exports = function(router, connection) {
     router.route('/log')
         .post(function(req, res) {
+	    console.log(req.body);
             var date     = new Date();
             var query    = "INSERT INTO ?? \
-                            (?,?,?,?,?,?,?,?,?,?,?,?) \
-                            VALUES (??,??,??,??,??,??,??,??,??,??,??,??)";
+                            (??,??,??,??,??,??,??,??,??,??,??,??) \
+                            VALUES (?,?,?,?,?,?,?,?,?,?,?,?)";
             var table    = ['tp_control_logs.events',
                             'ip', 'country_code', 'country_name', 'region_name', 'zip_code', 'time_zone', 'lattitude', 'longitude', 'action', 'user_id', 'username', 'event_datetime',
-                            req.body.ip, req.body.country_code, req.body.country_name, req.body.region_name, req.body.zip_code, req.body.time_zone, req.body.lattitude, req.body.longitude, req.body.action, req.body.user_id, req.body.username, date];
+                            req.body.ip, req.body.country_code, req.body.country_name, req.body.region_name, req.body.zip_code, req.body.time_zone, req.body.lattitude.toString(), req.body.longitude.toString(), req.body.action, req.body.user_id, req.body.username, date];
             query = mysql.format(query, table);
             connection.query(query, function(err, rows) {
                 if(err)
