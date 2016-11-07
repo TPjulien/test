b5e2357278b0246a65880d943fb679d7d5d07fd7
mahefa <nmahefa@gmail.com>
2016-11-07 16:59:51 +0100

    ajout d'un dossier de functions pour la propreté du code

--- server/app/routes/loginRoute.js
+++ server/app/routes/loginRoute.js
@@ -3,6 +3,7 @@ var jwt          = require('jsonwebtoken');
 var request      = require('request');
 var passport     = require('passport');
 var SamlStrategy = require('passport-saml').Strategy;
+require('./app/functions/token.js')
 require('dotenv').config({path: '/home/defaultuser/.env' });
 
 module.exports = function(router, connection, mysql) {
@@ -78,9 +79,6 @@ module.exports = function(router, connection, mysql) {
       passport.use(get_strategy);
     }
 
-    //passport.use(saml);
-
-
     // nouvelle version du password checker
     function checkPwUser(login, pwd, site_id, callback) {
       var query = "SELECT * FROM view_tpa_connexion WHERE LOGIN='" + login +  "' AND SITE_ID='" + site_id + "'";
@@ -100,38 +98,6 @@ module.exports = function(router, connection, mysql) {
           }
         }
       })
-    }
-
-    // fonction pour le login généralisé
-    function generate_token (column, result_column, result_site_id, can_logout, is_saml, req, res) {
-        var query = "SELECT * FROM ?? WHERE ?? = ? AND ?? = ? ORDER BY ?? LIMIT 1"
-        var table = ['profils.view_info_userConnected', column, result_column, 'SITE_ID', result_site_id, 'Role_ordre'];
-        query     = mysql.format(query, table);
-        connection.query(query, function(err, info_result) {
-            if (err) {
-                res.status(400).send(err);
-            } else if (info_result.length == 0) {
-                res.status(404).send("Not found");
-            } else {
-                var preToken = [{
-                  "site_id":              info_result[0].SITE_ID,
-                  "UID":                  info_result[0].UID,
-                  "DEPOSITED_DATE":       info_result[0].DEPOSITED_DATE,
-                  "home_community":       info_result[0].HomeCommunity,
-                  "username":             info_result[0].Login,
-                  "company":              info_result[0].SITE_LIBELLE,
-                  "firstname":            info_result[0].Customer_GivenName,
-                  "lastname":             info_result[0].Customer_surName,
-                  "user_auth":            info_result[0].Role,
-                  "can_logout":           can_logout,
-                  "is_saml":              is_saml
-                }];
-              var token = jwt.sign(preToken, 'travelSecret', {
-                  expiresIn: 7200
-              });
-              res.json({ 'token': token });
-            }
-        })
     }
 
     router.route('/redirect')
