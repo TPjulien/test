893ac8a944b3ca8e85f4d527e8e8e9f8f87db8a6
mahefa <nmahefa@gmail.com>
2016-11-07 16:45:06 +0100

    test de login

--- server/app/routes/loginRoute.js
+++ server/app/routes/loginRoute.js
@@ -1,15 +1,8 @@
-var http         = require('http');
-var jwt          = require('jsonwebtoken');
-var NodeRSA      = require('node-rsa');
-var http_post    = require('http-post');
-var request      = require('request');
-var key          = new NodeRSA({b: 512});
-var httpsRequest = require('https-request');
-var pythonShell  = require('python-shell');
-var cors         = require('cors')
-var SamlStrategy = require('passport-saml').Strategy;
-var passport     = require('passport');
 var fs           = require('fs');
+var jwt          = require('jsonwebtoken');
+var request      = require('request');
+var passport     = require('passport');
+var SamlStrategy = require('passport-saml').Strategy;
 require('dotenv').config({path: '/home/defaultuser/.env' });
 
 module.exports = function(router, connection, mysql) {
@@ -73,6 +66,7 @@ module.exports = function(router, connection, mysql) {
         table.sessionIndex        = profile.sessionIndex;
         table.nameQualifier       = profile.nameQualifier;
         table.spNameQualifier     = profile.spNameQualifier;
+        table.isSaml              = true;
         var token = jwt.sign(table, 'travelSecret', {
           expiresIn: 7200
         });
@@ -211,24 +205,15 @@ module.exports = function(router, connection, mysql) {
       })
     })
 
-    // mise à jour du login
-    router.route('/login')
-    .post (function (req, res) {
-      checkPwUser(req.body.username, req.body.password, req.body.SITE_ID, function(err, data) {
-        // dans le cas ou on a une erreur
-        if (err) {
-            res.status(401).send("Non autorisé");
-        } else {
-          if (data.length != 0) {
-            var query = 'SELECT * FROM ?? WHERE ?? = ? AND ?? = ? ORDER BY ?? LIMIT 1';
-            var table = ['profils.view_info_userConnected', 'site_id', req.body.SITE_ID, 'Login', req.body.username, 'Role_ordre'];
-
-            // var table = ['profils.view_info_userConnected','SITE_ID',req.body.SITE_ID,"Login",req.body.username];
-            query     = mysql.format(query, table);
-            connection.query(query, function(error, info_result) {
-              if (err) {
+    // fonction pour le login généralisé
+    function generate_token (column, result_column, result_site_id, can_logout, is_saml) {
+        var query = "SELECT * FROM ?? WHERE ?? = ? AND ?? = ? ORDER BY ?? LIMIT 1"
+        var table = ['profils.view_info_userConnected', column, result_column, 'SITE_ID', result_site_id, 'Role_ordre'];
+        query     = mysql.format(query, table);
+        connection.query(query, function(err, info_result) {
+            if (err) {
                 res.status(400).send(err);
-              } else {
+            } else {
                 var preToken = [{
                   "site_id":              info_result[0].SITE_ID,
                   "UID":                  info_result[0].UID,
@@ -239,23 +224,30 @@ module.exports = function(router, connection, mysql) {
                   "firstname":            info_result[0].Customer_GivenName,
                   "lastname":             info_result[0].Customer_surName,
                   "user_auth":            info_result[0].Role,
-                  "can_logout":           true,
-                  "is_saml":              false
+                  "can_logout":           can_logout,
+                  "is_saml":              is_saml
                 }];
-                var token = jwt.sign(preToken, 'travelSecret', {
+              var token = jwt.sign(preToken, 'travelSecret', {
                   expiresIn: 7200
-                });
-                res.json({
-                  token: token
-                });
-              }
-            })
-          } else {
-            res.status(400).send("user not found");
-          }
+              });
+              res.json({ 'token': token });
+            }
+        })
+    }
+    // mise à jour du login
+    router.route('/login')
+    .post (function (req, res) {
+      checkPwUser(req.body.username, req.body.password, req.body.SITE_ID, function(err, data) {
+        // dans le cas ou on a une erreur
+        if (err) {
+            res.status(401).send("Bad credential");
+        } else if (data.length != 0) {
+            generate_token('Login', req.body.username, req.body.SITE_ID, true, false);
+        } else {
+          res.status(400).send("user not found");
         }
       });
-    })
+    });
 
     router.route('/verify/:Login/:SITE_ID')
     .post (function(req, res) {
