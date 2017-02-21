699f267f890aeff82840c8b8a2e8c89c4ba7b23a
defaultuser <Preprod@portail.test>
2017-02-21 11:41:54 +0100

    query bus builder!

--- server/app/functions/busQueryBuilder.js
+++ server/app/functions/busQueryBuilder.js
@@ -1,13 +1,16 @@
 module.exports = {
-    queryBusBuilder: function(_stationsStart, _stationEnd, _api, _clientApi) {
+    queryBusBuilder: function(_stationsStart, _stationsEnd, _api, _clientApi) {
         // le début de l'url avec les parametres
         var urlText="https://api.distribusion.com:443/reseller/v2/connections/find?affiliate_partner_number=" + _clientApi + "&api_key=" + _api + ""; 
         // on fait une boucle pour faire tout cela
         for (var station in _stationsStart) {
-            urlText += "&departure_station_ids[]=" + _stationStart[station].id;
+	    console.log("le depart", _stationsStart[station].id);
+            urlText += "&departure_station_ids[]=" + _stationsStart[station].id;
         }
-        for (var station in _stationEnd) {
-            urlText += "&arrival_station_ids[]=" + _stationEnd[station].id;
+	console.log("la variable", _stationsEnd);
+        for (var station in _stationsEnd) {
+	    console.log("l'arrivé ", _stationsEnd[station].id);
+            urlText += "&arrival_station_ids[]=" + _stationsEnd[station].id;
         }
         return urlText;
     }
--- server/server.js
+++ server/server.js
@@ -43,7 +43,7 @@ function handleDisconnect() {
         password: process.env.DB_PASSWORD,
         database: process.env.DB_DATABASE,
         port:     process.env.DB_PORT,
-        debug:    true
+        debug:    false
     });
 
     connection.connect(function(err) {
